134a781220545559f820336bf0f9e0193f511b9d
Matthew Harrilal <matthewharrilal@gmail.com>
2018-02-01 14:51:22 -0800

    Made the database and configured the database.yml file

---
+++ CompanioBackend/app/models/attendance.rb
@@ -0,0 +1,4 @@
+class Attendance < ApplicationRecord
+    # In our migration file are the attributes for the attendance class which are the name of the user as well as 
+    # the in time and the out time
+end
---
+++ CompanioBackend/db/migrate/201802012240401_create_attendances.rb
@@ -0,0 +1,11 @@
+class CreateAttendances < ActiveRecord::Migration[5.1]
+  def change
+    create_table :attendances do |t|
+      t.string :name
+      t.string :in_time
+      t.string :out_time
+
+      t.timestamps
+    end
+  end
+end
