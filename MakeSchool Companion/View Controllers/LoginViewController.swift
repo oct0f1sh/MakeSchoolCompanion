46122686c579288011ea63770337414aa91191d4
Duncan <buzzbuzz.macdonald@gmail.com>
2018-02-02 11:38:38 -0800

    checkpoint

---
+++ MakeSchool Companion/View Controllers/LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  MakeSchool Companion
+//
+//  Created by Duncan MacDonald on 2/2/18.
+//  Copyright © 2018 Duncan MacDonald. All rights reserved.
+//
+
+import Foundation
