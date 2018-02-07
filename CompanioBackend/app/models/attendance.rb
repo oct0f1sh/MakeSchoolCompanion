acf4a5b3bd8514218c045ebb98ab73c0169a53b6
Matthew Harrilal <matthewharrilal@gmail.com>
2018-02-07 10:41:36 -0800

    Removed server folder

--- CompanioBackend/app/models/attendance.rb
+++
@@ -1,4 +0,0 @@
-class Attendance < ApplicationRecord
-    # In our migration file are the attributes for the attendance class which are the name of the user as well as 
-    # the in time and the out time
-end
