86ee7006214d7bf27c03fbe0b95dbbb00c8e67d1
Matthew Harrilal <matthewharrilal@gmail.com>
2018-05-09 18:01:05 -0700

    Got rid of the button to set the cookies

--- MakeSchool Companion/View Controllers/WebViewController.swift
+++ MakeSchool Companion/View Controllers/WebViewController.swift
@@ -20,67 +20,52 @@ class WebViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        backButton()
-    }
-    
-  
-    
-    func backButton() {
-        let image = UIImage(named: "icon-back") as UIImage?
-        let button = UIButton(type: UIButtonType.custom) as UIButton
-        let screenSize: CGRect = UIScreen.main.bounds
-        let screenWidth = screenSize.width
-        let screenHeight = screenSize.height
-        
-        button.frame = CGRect(x:screenWidth * 0.85, y:screenHeight * 0.90, width:56, height:56) // (X, Y, Height, Width)
-        button.setImage(image, for: .normal)
-        button.addTarget(self, action:#selector(customGoBack(sender:)), for: .touchUpInside)
-        self.view.addSubview(button)
-    }
-    
-    @objc func customGoBack(sender: UIButton) {
-        if self.webView.canGoBack {
-            print("Can go back")
-            self.webView.goBack()
-            self.webView.reload()
-        } else {
-            let storage = WKWebsiteDataStore.default().httpCookieStore.getAllCookies { (cookies) in
-                for cookie in cookies {
-                    if cookie.domain == "www.makeschool.com" && cookie.name == "_makeschool_session"{
-                        // Setting the cookie value for the session in keychain
-                        keychain.set(cookie.value, forKey: "cookieValue")
-                        keychain.set(cookie.domain, forKey: "cookieDomain")
-                        keychain.set(cookie.name, forKey: "cookieName")
-                        HTTPCookieStorage.shared.setCookie(cookie)
-                        
-                    }
-                }
-                showFacebookUserProfile(controller: self, completionHandler: { (response) in
-                    let beaconNetworkingLayer = BeaconNetworkingLayer()
-                    beaconNetworkingLayer.fetchBeaconData(route: .facebookCallback(email: StaticProperties.email, firstName: StaticProperties.firstName, lastName: StaticProperties.lastName, imageUrl: StaticProperties.imageUrl), completionHandler: { (user, response) in
-                        print("This is the response of the callback we made from facebook \(response)")
-                
-                        let idView = UIStoryboard(name: "Main", bundle: .main).instantiateInitialViewController() as! IDViewController
-                        UserDefaults.standard.set(true, forKey: "LoggedIn")
-                        DispatchQueue.main.async {
-                            self.present(idView, animated: true, completion: nil)
-                        }
-                    }, requestRoute: .postReuqest)
-                })
-    
-              
-             
             }
-            print("This is the value for the make school cookie \(storage)")
+
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        
+        if (navigationAction.navigationType == .linkActivated){
+            decisionHandler(.cancel)
+        } else {
+            decisionHandler(.allow)
+            let url = navigationAction.request.url?.absoluteURL
+            //handle your events from here
+            
+            if url == URL(string: "https://www.makeschool.com/dashboard#_=_") {
+                let storage = WKWebsiteDataStore.default().httpCookieStore.getAllCookies { (cookies) in
+                    for cookie in cookies {
+                        if cookie.domain == "www.makeschool.com" && cookie.name == "_makeschool_session"{
+                            // Setting the cookie value for the session in keychain
+                            keychain.set(cookie.value, forKey: "cookieValue")
+                            keychain.set(cookie.domain, forKey: "cookieDomain")
+                            keychain.set(cookie.name, forKey: "cookieName")
+                            HTTPCookieStorage.shared.setCookie(cookie)
+                            
+                        }
+                    }
+                    
+                    
+                    showFacebookUserProfile(controller: self, completionHandler: { (response) in
+                        let beaconNetworkingLayer = BeaconNetworkingLayer()
+                        beaconNetworkingLayer.fetchBeaconData(route: .facebookCallback(email: StaticProperties.email, firstName: StaticProperties.firstName, lastName: StaticProperties.lastName, imageUrl: StaticProperties.imageUrl), completionHandler: { (user, response) in
+                            print("This is the response of the callback we made from facebook \(response)")
+                            
+                            let idView = UIStoryboard(name: "Main", bundle: .main).instantiateInitialViewController() as! IDViewController
+                            UserDefaults.standard.set(true, forKey: "LoggedIn")
+                            DispatchQueue.main.async {
+                                self.present(idView, animated: true, completion: nil)
+                            }
+                        }, requestRoute: .postReuqest)
+                    })
+                    
+                    
+                    
+                }
+            }
         }
+       
     }
-    
-    // must be internal or public.
-    @objc func update() {
-        // Something cool
-    }
-    
+   
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
